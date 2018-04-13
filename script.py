f0b030521091c04d1b7274c829aca9ef72286cd2
SamesOfAm <flo.tepelmann@ewetel.net>
2018-04-13 13:56:38 +0200

    Add files via upload

--- script.py
+++ script.py
@@ -3,102 +3,45 @@
 import json
 import io
 import re
-from xdtools import XDFile
-
-def parse_name(child, placeOfNextDot, subKey):
-    if child.name[:4] == "$T$ ":
-        noBreakHyphens = re.sub(r"-\n", "", child.raw_text)
-        if noBreakHyphens != child.raw_text:
-            print "!!! Attention !!! Is this correct? " + noBreakHyphens
-        formattedText = re.sub(r"\n", " ", noBreakHyphens)
-        firstKey = child.name[placeOfNextDot:child.name.find(".", placeOfNextDot)]
-        placeOfNextDot = child.name.find('.', placeOfNextDot) + 1
-        if firstKey in database and firstKey != "":
-            secondKey = child.name[placeOfNextDot:child.name.find(".", placeOfNextDot)]
-            if secondKey in database[firstKey] and secondKey != "":
-                placeOfNextDot = child.name.find('.', placeOfNextDot) + 1
-                thirdKey = child.name[placeOfNextDot:child.name.find(".", placeOfNextDot)]
-                if thirdKey in database[firstKey][secondKey] and thirdKey != "":
-                    placeOfNextDot = child.name.find('.', placeOfNextDot) + 1
-                    fourthKey = child.name[placeOfNextDot:child.name.find(".", placeOfNextDot)]
-                    if fourthKey in database[firstKey][secondKey][thirdKey] and fourthKey != "":
-                        placeOfNextDot = child.name.find('.', placeOfNextDot) + 1
-                        fifthKey = child.name[placeOfNextDot:child.name.find(".", placeOfNextDot)]
-                        database[firstKey][secondKey][thirdKey][fourthKey][fifthKey] = formattedText
-                        print "Added to translations: " + formattedText
-                    elif fourthKey != "" and fourthKey != None:
-                        placeOfNextDot = child.name.find('.', placeOfNextDot) + 1
-                        fifthKey = child.name[placeOfNextDot:child.name.find(".", placeOfNextDot)]
-                        if fifthKey != None and fifthKey != "":
-                            database[firstKey][secondKey][thirdKey][fourthKey] = {fifthKey: formattedText}
-                            print "Added to translations: " + formattedText
-                        else:
-                            database[firstKey][secondKey][thirdKey][fourthKey] = formattedText
-                            print "Added to translations: " + formattedText
-                    else:
-                        database[firstKey][secondKey][thirdKey] = formattedText
-                        print "Added to translations: " + formattedText
-                elif thirdKey != "" and thirdKey != None:
-                    placeOfNextDot = child.name.find('.', placeOfNextDot) + 1
-                    fourthKey = child.name[placeOfNextDot:child.name.find(".", placeOfNextDot)]
-                    if fourthKey != None and fourthKey != "":
-                        placeOfNextDot = child.name.find('.', placeOfNextDot) + 1
-                        fifthKey = child.name[placeOfNextDot:child.name.find(".", placeOfNextDot)]
-                        if fifthKey != None and fifthKey != "":
-                            database[firstKey][secondKey][thirdKey] = {fourthKey: {fifthKey: formattedText}}
-                            print "Added to translations: " + formattedText
-                        else:
-                            database[firstKey][secondKey][thirdKey] = {fourthKey: formattedText}
-                            print "Added to translations: " + formattedText
-                    else:
-                        database[firstKey][secondKey][thirdKey] = formattedText
-                        print "Added to translations: " + formattedText
-                else:
-                    database[firstKey][secondKey] = formattedText
-                    print "Added to translations: " + formattedText
-            elif secondKey != "" and secondKey != None:
-                placeOfNextDot = child.name.find('.', placeOfNextDot) + 1
-                thirdKey = child.name[placeOfNextDot:child.name.find(".", placeOfNextDot)]
-                if thirdKey != None and thirdKey != "":
-                    placeOfNextDot = child.name.find('.', placeOfNextDot) + 1
-                    fourthKey = child.name[placeOfNextDot:child.name.find(".", placeOfNextDot)]
-                    if fourthKey != None and fourthKey != "":
-                        placeOfNextDot = child.name.find('.', placeOfNextDot) + 1
-                        fifthKey = child.name[placeOfNextDot:child.name.find(".", placeOfNextDot)]
-                        if fifthKey != None and fifthKey != "":
-                            database[firstKey][secondKey] = {thirdKey: {fourthKey: {fifthKey: formattedText}}}
-                            print "Added to translations: " + formattedText    
-                        else:
-                            database[firstKey][secondKey] = {thirdKey: {fourthKey: formattedText}}
-                            print "Added to translations: " + formattedText
-                    else:
-                        database[firstKey][secondKey] = {thirdKey: formattedText}
-                        print "Added to translations: " + formattedText
-                else:
-                    database[firstKey][secondKey] = formattedText
-                    print "Added to translations: " + formattedText
-            else:
-                database[firstKey] = formattedText
-                print "Added to translations: " + formattedText
-
-def look_for_texts(array):
-    for child in array:
-        if child.type == 'text':
-            parse_name(child, 4, None)
-        elif child.type == 'group':
-            look_for_texts(child.children)
-            
-jsonFile = open("db.json").read()
+from xdtools import XDFile            
+jsonFile = open("output.json").read()
 database = json.loads(jsonFile)
+prefix = "$T$ "
+inputFile = 'test4.xd'
+outputFile = 'output.json'
 
-with XDFile('test4.xd', 'r') as xd:
+def write_to_key(database, keys, counter, obj):
+    if keys[counter+1]:
+        database[keys[counter]] = {}
+        return write_to_key(database[keys[counter]], keys, counter+1, obj)
+    else:
+        database[keys[counter]] = obj.raw_text
+        print "Added to translation: " + keys[counter] + ": " + obj.raw_text
+    return counter
+
+def check_for_key(database, keys, counter, obj):
+    if keys[counter] and keys[counter] is not None and not "":
+        if keys[counter] in database:
+            return check_for_key(database[keys[counter]], keys, counter +1, obj)
+        else:
+            write_to_key(database, keys, counter, obj)
+        return counter
+        
+def look_for_texts(obj):
+    if obj.type == 'text' and obj.name[:len(prefix)] == prefix:
+        obj.raw_text = re.sub(r"-\n", "", obj.raw_text)
+        obj.raw_text = re.sub(r"\n", " ", obj.raw_text)
+        obj.name = re.sub("\$T\$ ", "", obj.name)
+        keys = obj.name.split('.')
+        check_for_key(database, keys, 0, obj)
+    elif obj.type == 'group':
+        for child in obj.children:
+            look_for_texts(child)
+
+with XDFile(inputFile, 'r') as xd:
     for artboard in xd.artboards:
-        for child in artboard.artwork:
-            if child.type == 'text':
-                parse_name(child, 4, None)
-            elif child.type == 'group':
-                look_for_texts(child.children)
-    # look_for_texts(xd.artworks)
+        for obj in artboard.artwork:
+            look_for_texts(obj)
 
-with io.open('output.json', 'w', encoding='utf-8') as outfile:
+with io.open(outputFile, 'w', encoding='utf-8') as outfile:
     outfile.write(unicode(json.dumps(database, indent=4, ensure_ascii=False)))
